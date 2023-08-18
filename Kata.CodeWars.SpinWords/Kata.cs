8cabd321fe891bfc017d919180d12b67cd1a55c5
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-08-18 00:30:10 -0500

    Add project files.

---
+++ Kata.CodeWars.SpinWords/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.CodeWars.SpinWords
+{
+    public class Kata
+    {
+        public static string SpinWords(string sentence)
+        {
+            if (string.IsNullOrEmpty(sentence))
+                throw new ArgumentException(nameof(sentence) + " is null or empty.");
+
+            List<string> words = sentence.Split(' ', StringSplitOptions.None).ToList();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (string.IsNullOrEmpty(words[i]))
+                    continue;
+
+                if (words[i].Count() < 5)
+                    continue;
+
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
